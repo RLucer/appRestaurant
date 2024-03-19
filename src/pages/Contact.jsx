426fc85f4a9a885e8bf97c3b2d77a675ed82d537
RLucer <lucero.ricardo@gmail.com>
2024-03-19 00:47:53 -0300

    added routes for deploy

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import FormContact from '../component/FormContact'
 import { Col, Container, Row } from 'react-bootstrap'
 import { Imagen } from "../component/Imagen";
+import SimpleMap from '../component/maps';
 export const Contact = () => {
   return (
     <>
       <Container className="mt-2">
         <Row>
-        <Col sm={7} >
-            <Imagen 
+        <Col sm={8} >
+        <SimpleMap />
+            {/* <Imagen 
               src={"./image/photo_contact.jpg"}
               width={"75%"}
               height={"auto"}
-            />
+            /> */}
           </Col>
-          <Col sm={5} >
+          <Col sm={4} >
             <FormContact />
           </Col>
         </Row>
